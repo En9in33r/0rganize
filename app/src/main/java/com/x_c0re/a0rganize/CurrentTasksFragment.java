ca2d5bd0fbdb32e18f4b081110aa186ea55e3fe9
rev0lt <semenvad1@gmail.com>
2017-08-16 13:12:02 +0800

    first

---
+++ app/src/main/java/com/x_c0re/a0rganize/CurrentTasksFragment.java
@@ -0,0 +1,21 @@
+package com.x_c0re.a0rganize;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CurrentTasksFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.current_tasks_fragment, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/x_c0re/a0rganize/SettingsFragment.java
@@ -0,0 +1,20 @@
+package com.x_c0re.a0rganize;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SettingsFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.settings_fragment, container, false);
+        return view;
+    }
+}
